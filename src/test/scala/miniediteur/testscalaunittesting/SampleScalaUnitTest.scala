510204631656c2734ec12a0c51d2ca98a2202ed9
allinard <al.linard@gmail.com>
2013-11-06 19:20:07 +0100

    Adding simple UT

---
+++ src/test/scala/miniediteur/testscalaunittesting/SampleScalaUnitTest.scala
@@ -0,0 +1,21 @@
+package miniediteur.testscalaunittesting
+
+import junit.framework._
+import org.junit.Assert._
+
+class SampleScalaUnitTest extends TestCase {
+ override def setUp() = {
+ }
+
+ override def tearDown() = {
+ }
+
+ def testOne() = {
+   assertEquals(1, 1)
+ }
+ 
+ def testTwo() = {
+	 assertEquals(2, 3)
+ }
+ 
+}
