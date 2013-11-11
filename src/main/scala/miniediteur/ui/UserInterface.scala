4868e609e58c9d2be156dd56ddcd4cb54178df1c
allinard <al.linard@gmail.com>
2013-11-11 18:57:41 +0100

    Comments

--- src/main/scala/miniediteur/ui/UserInterface.scala
+++ src/main/scala/miniediteur/ui/UserInterface.scala
@@ -13,40 +13,47 @@ import event._
 object UserInterface extends SimpleSwingApplication {
 
 	/**
-	 * 
+	 * Variable containing the copied value in the text editor
 	 */
 	var copiedValue: String = ""
 	
 	/**
-	 *
+	 * @TODO For Saved Actions
 	 */
 	var debut: Int = 0
 
 	/**
-	 *
+	 * @TODO For Saved Actions
 	 */
 	var fin: Int = 0
 
 	/**
-	 *
+	 * The UI
 	 */
 	def top = new MainFrame {
 
+		//Title of the frame
 		title = "MiniEditeur GL"
 
+		//Object containing the text area (defined size between brackets)
 		object textarea extends TextArea { columns = 50; rows = 10 }
 
+		//Variable for the Copy Button
 		val buttonCopy = new Button {
 			text = "Copy"
 		}
+		
+		//Variable for the Paste Button
 		val buttonPaste = new Button {
 			text = "Paste"
 		}
-
+		
+		//Variable for the Cut Button
 		val buttonCut = new Button {
 			text = "Cut"
 		}
 
+		//Components of the Frame
 		contents = new FlowPanel {
 			contents += new Label(" Editeur de texte ")
 			contents += buttonCopy
@@ -56,8 +63,10 @@ object UserInterface extends SimpleSwingApplication {
 			border = Swing.EmptyBorder(15, 10, 10, 10)
 		}
 		
+		//Adding listeners on buttons
 		listenTo(buttonCopy, buttonPaste, buttonCut)
 		
+		//Implementing actions
 		reactions += {
 			case ButtonClicked(`buttonCopy`) =>
 		      copiedValue = textarea.selected
