6e204f1f2069093808da1b8cb2fc152b8d799675
allinard <al.linard@gmail.com>
2013-11-18 15:54:35 +0100

    UT for memento (not implemented yet)

---
+++ src/test/scala/miniediteur/mem/CaretakerTest.scala
@@ -0,0 +1,22 @@
+package miniediteur.mem
+import junit.framework._
+import org.junit.Assert._
+import miniediteur.command.Copy
+import miniediteur.ui.UserInterface
+import miniediteur.memento._
+
+class CaretakerTest extends TestCase  {
+
+	override def setUp() = {
+	
+	}
+
+	override def tearDown() = {
+	
+	}
+	
+	def test1() = {
+		Assert.fail("Not implemented yet")
+	}
+	
+}
